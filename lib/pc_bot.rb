661c5bcb45803dcaef552b755829ff1073cad05f
jurgen1c <jurgen1c@gmail.com>
2020-09-06 11:25:11 -0600

    Setup chatterbot

--- lib/pc_bot.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'chatterbot/dsl'
-
-class PCbot < Chatterbot
-  def initialize
-    tweet
-  end
-end
---
+++ lib/whistle-bot.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+require 'chatterbot/dsl'
+
+tweet 'Hello world'
